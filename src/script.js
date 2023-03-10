ceb29b346e3741be5caa6ae8ee590ca34e0ff35f
a-maczynska <a.maczynskaa@gmail.com>
2023-03-10 18:52:25 +0100

    function is changed to arrow function

--- src/script.js
+++ src/script.js
@@ -1,9 +1,8 @@
-function change() {
+const change = () => {
   let button = document.querySelector("#button");
 
   button.innerText === "Kliknij tutaj 🍁"
     ? (button.innerHTML = "Hello, have a nice day! 🍁🍂🌻 ")
     : (button.innerHTML = "Kliknij tutaj 🍁");
-}
-
+};
 button.addEventListener("click", change);
